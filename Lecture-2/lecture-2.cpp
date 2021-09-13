71353a7c492e703cbfcae9236620648e71718fc8
Avinash Kumar <79476275+codeByte30@users.noreply.github.com>
2021-09-13 21:28:27 +0530

    Lecture-2

---
+++ Lecture-2/lecture-2.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI 3.14
+// float PI = 3.14;
+// typedef long long int ll;
+// void fun() {
+//     cout << "Hello\n";
+// }
+//argument and paramter
+// void fun(string name) {
+//     cout << "Hello " << name << endl;
+// }
+
+//no argument but has parameter
+// void fun(string name = "aman") {
+//     cout << "Hello " << name << endl;
+// }
+
+// int fun() {
+//     return 5;
+// }
+
+// bool prime(int n) {
+//     int count = 0;
+//     for (int i = 1; i <= n; i++) {
+//         if (n % i == 0) {
+//             count++;
+//         }
+//     }
+//     if (count > 2) {
+//         return false;
+//     }
+//     return true;
+// }
+//better complexity
+// bool prime(int n) {
+//     int count = 0;
+//     for (int i = 2; i <= sqrt(n); i++) {
+//         if (n % i == 0) {
+//             count++;
+//         }
+//     }
+//     if (count > 2) {
+//         return false;
+//     }
+//     return true;
+// }
+
+
+int fun() {
+    cout << "Hello\n";
+    return 1;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // int a, b;
+    // cin >> a >> b;
+    // char ch;
+    // cin >> ch;
+    // switch (ch) {
+    // case '+':
+    //     cout << a + b << endl;
+    //     break;
+    // case '-':
+    //     cout << abs(a - b) << endl;
+    //     break;
+    // case '*':
+    //     cout << a*b << endl;
+    //     break;
+    // case '/':
+    //     cout << a / b << endl;
+    //     break;
+    // }
+
+    // int r;
+    // cin >> r;
+    // cout << PI*r*r << endl;
+    // int r, h;
+    // cin >> r >> h;
+    // cout << 2 * PI*r*h << endl;
+
+    // ll a,b;
+    // cin>>a>>b;
+
+
+    // struct student {
+    //     int rollNumber;
+    //     string name;
+    // };
+
+    // student s;
+
+    // cin >> s.rollNumber >> s.name;
+
+
+    // cout << "Roll Number : " << s.rollNumber << endl;
+    // cout << "Name : " << s.name << endl;
+
+    // no argument no parameter
+
+    // fun();
+
+    // cout << fun();
+
+
+
+    int n;
+    // cin >> n;
+    // if (prime(n)) {
+    //     cout << "prime\n";
+    // }
+    // else {
+    //     cout << "not prime\n";
+    // }
+
+    // // prime
+
+
+    // int b;
+    // cin >> b;
+
+    // //prime
+    // if (prime(b)) {
+    //     cout << "prime\n";
+    // }
+    // else {
+    //     cout << "not prime\n";
+    // }
+    int a = fun();
+
+
+
+
+
+
+
+    return 0;
+}
